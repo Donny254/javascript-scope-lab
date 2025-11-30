ec8ac2677050ec3e1e68c331e3f8d4589b4115cf
don <donalduck254@gmail.com>
2025-11-30 08:10:14 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Write your solution in this file
+
+const burgers = ["Hamburger","Cheeseburger"]
+const featuredDrinks =["Strawberry Milkshake"]
+
+
+function addBurger(){
+    const newburger = "Flatburger";
+    return burgers.push(newburger);
+
+    if (true){
+        const anotherNewBurger ="Maple Bacon Burger";
+        return burgers.push(anotherNewBurger);
+        }
+}
+
+
+function changefeaturedDrink(featuredDrinks){
+
+    featuredDrinks = "The Javashake"
+
+}
+
+
