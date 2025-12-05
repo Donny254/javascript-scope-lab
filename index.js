5b49af9398c11389d393da539901c8f15389f536
don <donalduck254@gmail.com>
2025-12-05 18:35:11 +0300

    changes

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 // Write your solution in this file
-
-const burgers = ["Hamburger","Cheeseburger"]
-const featuredDrinks =["Strawberry Milkshake"]
-
+const burgers = ["Hamburger","Cheeseburger"];
+let featuredDrink ="Strawberry Milkshake";
 
 function addBurger(){
-    const newburger = "Flatburger";
-    return burgers.push(newburger);
-
-    if (true){
-        const anotherNewBurger ="Maple Bacon Burger";
-        return burgers.push(anotherNewBurger);
-        }
+    const newBurger = 'Flatburger';
+     burgers.push(newBurger);
 }
+    if (true){
+        const anotherNewBurger ='Maple Bacon Burger';
+        burgers.push(anotherNewBurger);
+        }
+ console.log(burgers);
 
-
-function changefeaturedDrink(featuredDrinks){
-
-    featuredDrinks = "The Javashake"
-
+function changefeaturedDrink(){
+    featuredDrink = 'The Javashake'
 }
 
 
