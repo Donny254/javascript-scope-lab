b0f1c70b14f8fd30ba518aee3153bf08fe5c7d71
don <donalduck254@gmail.com>
2025-12-05 19:17:35 +0300

    changes

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const burgers = ["Hamburger","Cheeseburger"];
 let featuredDrink ="Strawberry Milkshake";
 
 function addBurger(){
-    const newBurger = 'Flatburger';
+    const newBurger = "Flatburger";
      burgers.push(newBurger);
 }
     if (true){
-        const anotherNewBurger ='Maple Bacon Burger';
+        const anotherNewBurger ="Maple Bacon Burger";
         burgers.push(anotherNewBurger);
         }
  console.log(burgers);
 
-function changefeaturedDrink(){
-    featuredDrink = 'The Javashake'
+function changefeaturedDrink(){ 
+    featuredDrink = "The Javashake"
 }
 
 
