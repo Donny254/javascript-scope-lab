a2f908361dbd28eff25d7812dd41ac9f36366b91
don <donalduck254@gmail.com>
2025-12-15 12:36:18 +0300

    changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// Write your solution in this file
-const burgers = ["Hamburger","Cheeseburger"];
-let featuredDrink ="Strawberry Milkshake";
+// Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink="Strawberry Milkshake";
 
 function addBurger(){
-    const newBurger = "Flatburger";
-     burgers.push(newBurger);
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
 }
-    if (true){
-        const anotherNewBurger ="Maple Bacon Burger";
-        burgers.push(anotherNewBurger);
-        }
- console.log(burgers);
+if(true){
+    const anotherNewBurger ='Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+console.log(burgers);
 
-function changefeaturedDrink(){ 
-    featuredDrink = "The Javashake"
+function changeFeaturedDrink(){
+    featuredDrink ='The Javashake'
 }
 
 
